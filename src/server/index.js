865cb6be756079b3cac0a0d35e6c82e68ef220ee
Baranov Nikita <baranov.nikita@gmail.com>
2017-02-02 18:02:28 +0300

    port from env

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,6 @@ app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
-app.listen(3000, () => {
-	console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started');
 });
