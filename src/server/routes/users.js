53896c60dd08b5350c389f9c70dbc135e8bf181e
nikita <baranov.nikita@gmail.com>
2017-02-09 11:48:05 +0300

    rename route user file

---
+++ src/server/routes/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const User = require('../models/User');
+const lodash = require('lodash');
+const signup = require('../../share/validate/signup');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+	const errors = signup(req.body);
+
+	if (!lodash.isEmpty(errors)) {
+		return res.status(500).json({ errors });
+	}
+	return User.register(new User(req.body), req.body.password, (err, user) => {
+		if (err) {
+			if (err.name === 'UserExistsError') {
+				return res.status(500).json({ email: 'User with this email already exist!' });
+			}
+			return res.status(500).json({ error: 'Error!' });
+		}
+		return res.json({ success: true });
+	});
+});
+
+router.get('/:email', (req, res) => User
+	.findOne({ email: req.params.email })
+	.then(user => res.json({ user }))
+	.catch(err => res.json({ error: err }))
+);
+module.exports = router;
